6eb0962ac3499f6c9eb76560956d036cd8ea44d7
arietan <nnh.mike@gmail.com>
2018-02-01 23:21:00 +0800

    Update compile.sh

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -2,6 +2,13 @@
 
 cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive
 
+if [$DEPLOYMENT_GROUP_NAME == 'helloPhoenix-Staging'] 
+then
+  aws s3 cp s3://helloerlang-secretsbucket-7apezuluts7h/creds_staging.txt .
+  eval $(cat creds.txt | sed 's/^/export /')
+  rm creds.txt
+fi
+
 # Install deps
 mix local.hex --force
 mix local.rebar --force
