3e5b45cb35b02ef190adfd3101b862a03e69a967
arietan <nnh.mike@gmail.com>
2018-02-02 00:29:58 +0800

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive
-MIX_ENV=$CUR_ENV mix phx.server > /dev/null 2>&1 &
+mix phx.server > /dev/null 2>&1 &
