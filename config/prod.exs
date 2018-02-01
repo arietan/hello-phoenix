417d9f793f3146fa81cfdc34c2e19f01f5670ac3
arietan <nnh.mike@gmail.com>
2018-02-02 00:01:30 +0800

    Update prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -58,7 +58,12 @@ config :logger, level: :info
 #
 #     config :blog_phoenix, BlogPhoenixWeb.Endpoint, server: true
 #
+config :blog_phoenix, BlogPhoenixWeb.Endpoint,
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
+# Configure your database
+config :blog_phoenix, BlogPhoenixWeb.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
